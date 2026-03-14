c06672fb15d1e443815826e4482c16f4dc89ae00
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-03-14 21:44:30 +0900

    finish-162

--- JavaScript/app-if.js
+++ JavaScript/app-if.js
@@ -1,31 +1,54 @@
-console.log("条件分岐の前");
+//console.log("条件分岐の前");
+//
+//if (1 + 2 === 2) {
+//  console.log("計算してみる");
+//}
+//
+//console.log("条件分岐の後");
+//
+//let random = Math.random();
+//if (random < 0.5) {
+//  console.log("数字は0.5より小さい");
+//  console.log(random);
+//}
+//
+//const daiOfWeek = "Monday";
+//
+//if (daiOfWeek === "Monday") {
+//  console.log("月曜日は憂鬱");
+//} else if (daiOfWeek === "Saturday") {
+//  console.log("土曜日は最高");
+//}
+//
+//const age = 35;
+//
+//if (age < 5) {
+//  console.log("無料になります");
+//} else if (age < 10) {
+//  console.log("子供料金なので1000円になります");
+//} else if (age < 65) {
+//  console.log("大人料金なので2000円になります");
+//}
+//
+//const dayOfweekElse = prompt('英語で入力してください').toLocaleLowerCase();
+//
+//if (dayOfweekElse === "monday") {
+//  console.log("月曜日は憂鬱");
+//} else if (dayOfweekElse === "saturday") {
+//  console.log("土曜日は最高");
+//} else {
+//  console.log('はぁ。。。');
+//}
 
-if (1 + 2 === 2) {
-  console.log("計算してみる");
+const password = prompt('パスワードを入力してください');
+
+if (password.length >= 6) {
+  if (password.indexOf(' ') === -1) {
+    console.log('OK');
+  } else {
+    console.log('空白があります');
+  }
+} else {
+  console.log('パスワードが短すぎます。6文字以上にしてください')
 }
 
-console.log("条件分岐の後");
-
-let random = Math.random();
-if (random < 0.5) {
-  console.log("数字は0.5より小さい");
-  console.log(random);
-}
-
-const daiOfWeek = "Monday";
-
-if (daiOfWeek === "Monday") {
-  console.log("月曜日は憂鬱");
-} else if (daiOfWeek === "Saturday") {
-  console.log("土曜日は最高");
-}
-
-const age = 35;
-
-if (age < 5) {
-  console.log("無料になります");
-} else if (age < 10) {
-  console.log("子供料金なので1000円になります");
-} else if (age < 65) {
-  console.log("大人料金なので2000円になります");
-}
