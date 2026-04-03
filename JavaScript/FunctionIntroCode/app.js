105ac9416a214c6b50b538cef3f124522c3900b0
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-04-03 18:10:46 +0900

    finish-200

---
+++ JavaScript/FunctionIntroCode/app.js
@@ -0,0 +1,23 @@
+function singSong() {
+    console.log('ド');
+    console.log('レ');
+    console.log('ミ');
+    console.log('ファ');
+    console.log('ソ');
+    console.log('ラ');
+    console.log('シ');
+    console.log('ド');
+}
+singSong();
+
+function greet(firstName) {
+    console.log(`HI!, ${firstName}`);
+}
+greet('aya');
+
+function shout(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+shout('hello world');
