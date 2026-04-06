5a3a5b08222537e52bd5309d38f3341298cb6201
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-04-06 18:28:47 +0900

    finish-206

--- JavaScript/Scope/app.js
+++ JavaScript/Scope/app.js
@@ -14,3 +14,47 @@ function birdWatch() {
 //これは a
 //birdWatch();
 //console.log(bird);
+
+let redius = 8;
+// {} はブロックと呼ばれ、この中のスコープはこのブロック内に限定される。
+if (redius > 0) {
+    const PI = 3.14;
+    let area = PI * redius * redius;
+    console.log(area);
+}
+//これはエラー
+//console.log(PI);
+//console.log(msg);
+
+for (let i = 0; i < 10; i++) {
+    var msg = 'hello';
+}
+//varはブロックスコープではないのでこれはOKになる。基本、varは使わない。
+console.log(msg);
+
+function bankRobbery() {
+    const heroes = ['スパイダーマン', 'スーパーマン', 'ブラックパンサー'];
+    function help() {
+        for (let hero of heroes) {
+            console.log(`Help me! ${hero}!`);
+        }
+    }
+    //レキシカルスコープ コード上のどこで定義したかによってスコープが決まること
+    //これが無いとheroesが読み込めないのでbankRobbery()を実行してもundefindになる
+    help()
+}
+bankRobbery()
+
+//レキシカルスコープ  その②
+// あいうえお
+let xxx = 'あいうえお';
+function hoge() {
+    console.log(x);
+}
+
+//　これも　あいうえお　になる
+// hogeでは直前のxxxを読み込むので。
+function moge() {
+    let xxx = 'かきくけこ';
+    hoge();
+}
