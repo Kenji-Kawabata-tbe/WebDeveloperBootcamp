efa242e2a7bec09e181adc769656347c90e66736
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-04-12 19:50:19 +0900

    finish-220

--- JavaScript/ArrowFunctions/app.js
+++ JavaScript/ArrowFunctions/app.js
@@ -43,3 +43,34 @@ const add3 = (x, y) => x + y;
 //    const msg = 1;
 //    Math.floor(Math.random() * 6) +1
 //);
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+//const newMovies = movies.map(function (movie) {
+//    return `${movie.title} - ${movie.score / 10}`;
+//})
+
+//↑と同じものをarrow関数で
+const newMovies1 = movies.map(movie => (
+    `${movie.title} - ${movie.score / 10}`
+))
+const newMovies2 = movies.map(movie =>
+    `${movie.title} - ${movie.score / 10}`
+)
