1fa4756d5f42c72625ec49429fdc9eb42fa1342a
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-04-17 09:50:47 +0900

    finish-228

--- NEW_JS_FUTURES_CODE/DefaultParams/app.js
+++ NEW_JS_FUTURES_CODE/DefaultParams/app.js
@@ -1,4 +1,4 @@
-//デフォルト引数 パラメーターの後ろに=でデフォルト値を設定する
+////////////デフォルト引数 パラメーターの後ろに=でデフォルト値を設定する////////////
 function rollDie(numSides = 6) {
     return Math.floor(Math.random() * numSides) + 1;
 }
@@ -11,3 +11,34 @@ function greet(person, msg = 'こんにちは', suffix='!!') {
 //greet('山田', '。')
 //デフォルト値を使いたいパラメーターの引数をundefinedを指定する
 greet('山田', undefined, '。')
+
+
+
+
+
+
+
+////////////スプレッド構文////////////
+// ...がスプレッド構文
+// 配列だけでなく列挙可能なオブジェクト(順番に処理することができるオブジェクト)であれば利用可能
+
+//配列のスプレッド構文
+Math.max(13, 4, 5, 6, 21, 9, 21, 2222)
+Math.min(2, 5, 1)
+
+const nums = [13, 4, 5, 6, 21, 9, 21, 2222]
+// これはエラー。Math.maxは配列ではなく数字が欲しいから
+//Math.max(nums)
+Math.max(...nums)
+
+const cats = ['Tama', 'Tora', 'Momo'];
+const dogs = ['Hachi', 'Pochi'];
+cats.concat(dogs)
+
+// cats.conatactと同様に配列を合体させることもできる
+const allPets1 = [...cats, ...dogs];
+const allPets2 = [...cats, 'Sakura', ...dogs];
+
+const testMoji = ['あいうえお'];
+// こうすると一つ一つの文字が要素として展開された配列になる
+const testMoji2 = [...testMoji];
