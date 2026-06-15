7d80c2ac9ac7c5cb193984c4cc828ed7d3f58e27
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-06-16 08:44:53 +0900

    finish-355

--- Express/RESTful_Routes_CODE/RestDemo/index.js
+++ Express/RESTful_Routes_CODE/RestDemo/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+//フォームから渡ってきたデータをパースする
+//ポストで送られてくるデータには種類(フォーマット)がある。
+//受け取る側もそれを変換(パース)するかという事を意識しないといけない
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //GETリクエストを送る
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response');
@@ -8,9 +14,16 @@ app.get('/tacos', (req, res) => {
 
 //POST リクエストを送る
 app.post('/tacos', (req, res) => {
-    res.send('POST /tacos response');
+    //res.send('POST /tacos response');
+    //postされたデータの取得
+    console.log(req.body);
+    const { meat, qty } = req.body;
+    res.send(`${qty} ${meat} どうぞ`);
+    
 });
 
 app.listen(3000, () => {
     console.log('ポート3000で待ち受け中...');
 })
+
+
