e141fa2d60d400b7830cddec9526e79da67c01d8
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-07-12 20:29:38 +0900

    finish-413

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -23,11 +23,21 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: '私の庭', description: '気軽に安くキャンプ！！' });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
 });
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
+
+//app.get('/makecampground', async (req, res) => {
+//    const camp = new Campground({ title: '私の庭', description: '気軽に安くキャンプ！！' });
+//    await camp.save();
+//    res.send(camp);
+//});
 
 app.listen(3000, () => {
     console.log('ポート3000でリクエスト受付中...');
