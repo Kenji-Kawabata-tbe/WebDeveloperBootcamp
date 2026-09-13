75933fffbf9f53e8e123acdb74087ca250708079
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-09-13 13:56:58 +0900

    finish-485

---
+++ SESSIONDEMO/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+//express-sessionをapp.useすることで全てのリクエストにsessionが使える。
+//reqオブジェクトにreq.sessionというプロパティが用意されるようになる。
+//expressのデフォルトではサーバのMemoryStoreというメモリ領域に保存されるが、通常はこの使い方はしない。redisとかmongoが一般的。
+app.use(session({
+    secret: 'mysecret'
+}));
+
+//sessionが有効になっているとcookieにconnect.sidが設定される。sidはセッションidという意味。
+//セッションIDはcookieに保存されるのでブラウザを変えたりcookieを削除するとまた最初からセッションを作成する
+app.get('/viewcount', (req, res) => {
+    //res.send('あなたはX回このページを表示しました');
+    if(req.session.count) {
+        req.session.count += 1;
+    } else (
+        req.session.count = 1
+    )
+    res.send(`あなたは${req.session.count}回このページを表示しました`);
+});
+
+
+app.listen(3000, () => {
+    console.log('ポート3000で受付中...')
+});
