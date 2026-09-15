463705bb60155ebf1127cbc04b7fc8e06ebf4e26
KenjiKawabata <kawabatatbegoat@gmail.com>
2026-09-15 20:50:26 +0900

    finish-488

--- SESSIONDEMO/index.js
+++ SESSIONDEMO/index.js
@@ -5,9 +5,17 @@ const session = require('express-session');
 //express-sessionをapp.useすることで全てのリクエストにsessionが使える。
 //reqオブジェクトにreq.sessionというプロパティが用意されるようになる。
 //expressのデフォルトではサーバのMemoryStoreというメモリ領域に保存されるが、通常はこの使い方はしない。redisとかmongoが一般的。
-app.use(session({
-    secret: 'mysecret'
-}));
+//app.use(session({
+//    secret: 'mysecret'
+//}));
+
+const sessionOptions = {
+    secret: 'mysecret',
+    // デフォルトの設定だとwarningがでるので。
+    resave: false,
+    saveUninitialized: false
+};
+app.use(session(sessionOptions));
 
 //sessionが有効になっているとcookieにconnect.sidが設定される。sidはセッションidという意味。
 //セッションIDはcookieに保存されるのでブラウザを変えたりcookieを削除するとまた最初からセッションを作成する
@@ -20,6 +28,19 @@ app.get('/viewcount', (req, res) => {
     )
     res.send(`あなたは${req.session.count}回このページを表示しました`);
 });
+
+// usernameというquerystringを受け取ったらそれをセッションに保存する。
+// /register?username=ken  デフォルトはAnonymous
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+})
+// /registerの値を取得
+app.get('/greet', (req, res) => {
+    const { username  } = req.session;
+    res.send(`ようこそ、${username}さん`);
+})
 
 
 app.listen(3000, () => {
